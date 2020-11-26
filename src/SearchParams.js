27e8e0668a524d40fdfbeca90b43ab50b06ccb40
Ahmed Nasser <ahmednas197@gmail.com>
2020-11-26 22:37:07 +0200

    add useDropdown and fix some errors in useDropdonwn.js

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,31 +1,40 @@
-import React,{useState} from "react";
-import {ANIMALS  } from"@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-    
-    const [location , setLocation]= useState("Seattle , WA");
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, updateBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
 
-    
-    const [breeds, setBreeds] = useState([])
-    const [animal , AnimalDropdown] = useDropdown("Animal" , "dog", ANIMALS);
-    const [breed , BreedDropdown] = useDropdown("Breed", "",breeds);
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      updateBreeds(breedStrings);
+    }, console.error);
+  }, [animal]);
+
   return (
     <div className="search-params">
-        <h1>{location}</h1>
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" onChange={ event  => setLocation(event.target.value)} />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => updateLocation(e.target.value)}
+          />
         </label>
-       <AnimalDropdown />
-       <BreedDropdown />
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
   );
-
-
 };
 
 export default SearchParams;
--- src/useDropdown.js
+++ src/useDropdown.js
@@ -1,25 +1,28 @@
-import React , {useState}from "react";
+import React, { useState } from "react";
 
-const useDropdown = (label , defaultstate , option) => { 
-    const [state , setState] = useState(defaultState) ; 
-    const id = `use-dropdown-${label.replace(" " ,"").toLowerCase()}`
-        const Dropdown = () => (
+const useDropdown = (label, defaultState, options) => {
+  const [state, updateState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
+  const Dropdown = () => (
     <label htmlFor={id}>
-        {labed}
-            <select id = {id} 
-            value ={state}
-            onchange={e => setState(e.target.value)}
-            onBlur={e=> setState(e.target.value)}
-            disabled={option.length ===0 }
-
-            >
-                <option>All</option>
-                {option.map(item => (<option key = {item } value={item} >{item}</option>
-                ))}
-            </select>
-        </label>
-    );
-    return [state, Dropdown , setState];
+      {label}
+      <select
+        id={id}
+        value={state}
+        onChange={e => updateState(e.target.value)}
+        onBlur={e => updateState(e.target.value)}
+        disabled={!options.length}
+      >
+        <option />
+        {options.map(item => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+  return [state, Dropdown, updateState];
 };
 
 export default useDropdown;
